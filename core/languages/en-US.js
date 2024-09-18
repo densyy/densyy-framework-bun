92001d948c7c660b4f278d9b94ea89ce43d07e51
Densyy <densyy@hotmail.com>
2024-09-18 20:01:58 -0300

    add /core/languages/en-US

---
+++ core/languages/en-US.js
@@ -0,0 +1,12 @@
+export default {
+  middlewares: {
+    ids_1: 'The IDs are invalid.',
+    token_1: 'Authentication token is missing.',
+    token_2: 'Invalid authentication token.'
+  },
+  tools: {
+    response_1: 'There was an internal error. Please try again later or contact support.'
+  },
+  express_1: 'Server listening on port',
+  express_2: 'Route not found'
+}
