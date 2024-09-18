95931d006da21b27de7f273556f0fa5060d1b118
Densyy <densyy@hotmail.com>
2024-09-18 20:02:06 -0300

    add /core/languages/pt-BR

---
+++ core/languages/pt-BR.js
@@ -0,0 +1,12 @@
+export default {
+  middlewares: {
+    ids_1: 'Os IDs são inválidos.',
+    token_1: 'Token de autenticação inexistente.',
+    token_2: 'Token de autenticação inválido.'
+  },
+  tools: {
+    response_1: 'Houve um erro interno. Por favor tente novamente mais tarde ou entre em contato com o suporte.'
+  },
+  express_1: 'Servidor ouvindo na porta',
+  express_2: 'Rota inexistente'
+}
